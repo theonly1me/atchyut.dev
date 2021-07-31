cb9ad4d6bd63c8f03c6fdc9ad3511ef60362ea5b
Atchyut Preetham Pulavarthi <pulavarthi.preetham@gmail.com>
2021-08-01 04:41:43 +0530

    V2.0 initial commit

--- src/state/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import appReducer from '../state/slices/appSlice';
-
-export default configureStore({
-  reducer: {
-    app: appReducer,
-  },
-});
