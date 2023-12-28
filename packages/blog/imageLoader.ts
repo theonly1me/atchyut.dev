4acd530d2bbb2dd85e7db995b2c5e90053cd47fb
Atchyut Preetham Pulavarthi <pulavarthi.preetham@gmail.com>
2023-12-28 18:20:55 +0530

    chore: use ImageLoaderProps type

--- packages/blog/imageLoader.ts
+++ packages/blog/imageLoader.ts
@@ -1,11 +1,5 @@
-export default function imageLoader({
-  src,
-  width,
-  quality,
-}: {
-  src: string;
-  width: string | number;
-  quality: string | number;
-}) {
+import { ImageLoaderProps } from 'next/dist/shared/lib/image-config';
+
+export default function imageLoader({ src, width, quality }: ImageLoaderProps) {
   return `${src}?w=${width}&q=${quality || 75}`;
 }
