e5f1c1c12542d51bd7932bd08669286cabb73f02
Atchyut Preetham Pulavarthi <pulavarthi.preetham@gmail.com>
2023-12-28 19:15:50 +0530

    chore: fix styling issues with Card

--- packages/blog/src/app/components/Card.tsx
+++ packages/blog/src/app/components/Card.tsx
@@ -10,32 +10,24 @@ const Card: React.FC<Page> = ({ pageName, pageId, imageUrl, pageSummary }) => {
       onClick={() => {
         router.push(`/blogpost/${pageId}`);
       }}
-      className="border w-xs rounded-lg h-40 hover:translate-y-1 hover:scale-105 active:-translate-y-0 active:scale-100 transition-transform duration-300 cursor-pointer"
+      className="flex flex-col md:flex-row md:gap-2 border w-full min-h-52 md:w-1/2 md:h-52 rounded-lg hover:translate-y-1 hover:scale-105 active:-translate-y-0 active:scale-100 transition-transform duration-300 cursor-pointer"
     >
-      <div className="flex">
-        <div className="relative w-48">
-          <Image
-            src={imageUrl}
-            layout="fill"
-            objectFit="cover"
-            alt={`Cover image for ${pageName}`}
-            className="absolute object-cover rounded-l-lg"
-            placeholder="blur"
-            blurDataURL={imageUrl}
-            loader={imageLoader}
-            quality={75}
-          />
-        </div>
-        <div className="flex-auto px-4 py-4 flex-wrap max-w-xs h-40 ">
-          <div className="flex flex-wrap">
-            <h3 className="flex-auto text-lg font-semibold text-neutral-700">
-              {pageName}
-            </h3>
-          </div>
-          <div className="flex items-baseline mt-4 mb-4 py-4 border-t border-slate-200 text-neutral-600">
-            {pageSummary}
-          </div>
-        </div>
+      <Image
+        src={imageUrl}
+        width={400}
+        height={200}
+        alt={`Cover image for ${pageName}`}
+        className="rounded-t-lg md:rounded-l-lg md:rounded-tr-none md:w-64"
+        placeholder="blur"
+        blurDataURL={imageUrl}
+        loader={imageLoader}
+        quality={75}
+      />
+
+      <div className="p-4">
+        <h3 className="text-xl font-semibold text-neutral-700">{pageName}</h3>
+        <hr className="my-4" />
+        <div className="text-neutral-600">{pageSummary}h</div>
       </div>
     </div>
   );
